2ab74fbafdf734c05de919ac68e320a8611eeaff
Benjamin Nguyen <bennguyen8180@gmail.com>
2026-06-10 20:10:10 +1000

    snacks

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -51,7 +51,8 @@ return {
       layouts = {
         sidebar = {
           layout = {
-            position = "right"
+            position = "right",
+            width = 50
           }
         }
       },
@@ -142,7 +143,7 @@ return {
     { "<leader>gL",      function() Snacks.picker.git_log_line() end,                            desc = "Git Log Line" },
     { "<leader>gs",      function() Snacks.picker.git_status() end,                              desc = "Git Status" },
     { "<leader>gS",      function() Snacks.picker.git_stash() end,                               desc = "Git Stash" },
-    { "<leader>gd",      function() Snacks.picker.git_diff() end,                                desc = "Git Diff (Hunks)" },
+    -- { "<leader>gd",      function() Snacks.picker.git_diff() end,                                desc = "Git Diff (Hunks)" },
     { "<leader>gf",      function() Snacks.picker.git_log_file() end,                            desc = "Git Log File" },
 
     -- gh
